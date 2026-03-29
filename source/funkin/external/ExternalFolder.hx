a0a7cb969ae0ee9f5117241d77cc75e723313465
Kj20series <simplemojo55@gmail.com>
2026-03-29 20:04:07 +0800

    Update ExternalFolder.hx

--- source/funkin/external/ExternalFolder.hx
+++ source/funkin/external/ExternalFolder.hx
@@ -6,20 +6,15 @@ import lime.app.Application;
 class ExternalFolder
 {
   #if android
-  // On Android we put everything in public storage so the user can easily access mods/assets
   static final ENGINE_FOLDER:String = "/storage/emulated/0/.VSliceEngine";
   static final ASSETS_FOLDER:String = "/storage/emulated/0/.VSliceEngine/assets";
   static final MODS_FOLDER:String = "/storage/emulated/0/.VSliceEngine/mods";
   #else
-  // Desktop / other platforms keep the original relative folder next to the executable
   static final ENGINE_FOLDER:String = ".VSliceEngine";
   static final ASSETS_FOLDER:String = ".VSliceEngine/assets";
   static final MODS_FOLDER:String = ".VSliceEngine/mods";
   #end
 
-  /**
-   * Call this early in Main.hx to initialize the .VSliceEngine folder.
-   */
   public static function init():Void
   {
     if (!FileSystem.exists(ENGINE_FOLDER))
@@ -41,10 +36,13 @@ class ExternalFolder
 
   static function showSetupMessage():Void
   {
-    var message:String = "Please put the 'assets' and 'mods' folders inside the '.VSliceEngine' folder.\n\n"
-      + (lime.system.System.platform == lime.system.Platform.ANDROID
-          ? "Location: /storage/emulated/0/.VSliceEngine"
-          : "Location: next to the game executable");
+    #if android
+    var location:String = "/storage/emulated/0/.VSliceEngine";
+    #else
+    var location:String = "next to the game executable";
+    #end
+
+    var message:String = "Please put the 'assets' and 'mods' folders inside the '.VSliceEngine' folder.\n\nLocation: " + location;
 
     trace("[VSliceEngine] " + message);
     Application.current.window.alert(message, "VSlice Engine Setup");
